cf3816756b72793cfa3b87cbbd6b6c4d8678c121
Hiroshi Nakasone <mnakasone3@my.bcit.ca>
2023-11-03 13:06:21 -0700

    Added file handling code

--- A2.c
+++ A2.c
@@ -2,6 +2,40 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(){
-    printf("Hello World!\n");
+int main(char argc, char *argv[]){
+    if(argc != 4){
+        fprintf(stderr, "Usage: %s <input_file> <output_file> <option>\n", argv[0]);
+        return 1;
+    }
+
+    int option = atoi(argv[3]);
+    if(option < 1 || option > 3){
+        fprintf(stderr, "Invalid option: Choose 1, 2, or 3.\n");
+        return 1;
+    }
+
+    FILE *inputFile = fopen(argv[1], "r");
+    if(inputFile == NULL){
+        fprintf(stderr, "Error: Could not open input file\n");
+        return 1;
+    }
+
+    char *ANum = "A01178073_AXXXXXX_AXXXXXX"; // A numbers of everyone. AXXXX_AXXXX_AXXX format.
+    FILE *outputFile = fopen(ANum, "w");
+    if (outputFile == NULL) {
+        fprintf(stderr, "Failed to create the output file.\n");
+        fclose(inputFile);
+        return 1;
+    }
+
+    FILE *tempFile = fopen(argv[2], "w");
+    if (tempFile == NULL) {
+        fprintf(stderr, "Failed to create the temp file.\n");
+        fclose(inputFile);
+        return 1;
+    }
+
+        
+
+
 }
